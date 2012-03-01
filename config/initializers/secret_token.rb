79335fb1f57fb83b39d883cb9294397216163d82
Julian Karhoff <julian.karhoff@gmx.de>
2012-03-01 15:18:54 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trutra::Application.config.secret_token = '5a70dd4ec2414bf61af83081d7e655749462d6abb914d467dc5fac52d833c7f6a9644d37624343ccd17d94c172d89b7f1cc92f5b0525b4bf7f775c468b6cf120'
